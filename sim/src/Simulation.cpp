dec08c3642e412d94b8e565545024932de5019f8
PaulBailey-1 <paul.ryan.bailey@gmail.com>
2023-11-22 18:31:10 -0600

    added xml loading for map

--- sim/src/Simulation.cpp
+++ sim/src/Simulation.cpp
@@ -3,16 +3,6 @@
 
 #include "Simulation.h"
 
-ci::Shape2d makeRect(glm::vec2 topLeft, float width, float height) {
-	ci::Shape2d shape;
-	shape.moveTo(topLeft);
-	shape.lineTo(glm::vec2(topLeft.x + width, topLeft.y));
-	shape.lineTo(glm::vec2(topLeft.x + width, topLeft.y + height));
-	shape.lineTo(glm::vec2(topLeft.x, topLeft.y + height));
-	shape.close();
-	return shape;
-}
-
 Simulation::Simulation() {
 
 	if (_db.connect()) {
@@ -22,38 +12,7 @@ Simulation::Simulation() {
 
 	printf("Loading map ... ");
 
-	_map.size = {100, 60};
-
-	_map.inBounds.push_back(makeRect({0, 0}, 100, 60));
-	_map.outBounds.push_back(makeRect({10, 10}, 80, 40));
-
-	_map.devs.push_back(DeviceLoc{ 0, { 50, 5 }, 180 });
-	_map.devs.push_back(DeviceLoc{ 1, { 50, 5 }, 0 });
-	_map.devs.push_back(DeviceLoc{2, { 95, 30 }, 90});
-	_map.devs.push_back(DeviceLoc{3, { 95, 30 }, -90});
-	_map.devs.push_back(DeviceLoc{4, { 50, 55 }, 0});
-	_map.devs.push_back(DeviceLoc{5, { 50, 55 }, 180});
-	_map.devs.push_back(DeviceLoc{6, { 5, 30 }, -90});
-	_map.devs.push_back(DeviceLoc{7, { 5, 30 }, 90});
-
-	_map.doors.push_back(Door(0, { 20, 0 }, 0));
-	_map.doors.push_back(Door(1, { 40, 0 }, 0));
-	_map.doors.push_back(Door(2, { 60, 0 }, 0));
-	_map.doors.push_back(Door(3, { 80, 0 }, 0));
-
-	_map.doors.push_back(Door(4, { 100, 20 }, 90));
-	_map.doors.push_back(Door(5, { 100, 40 }, 90));
-
-	_map.doors.push_back(Door(6, { 80, 60 }, 0));
-	_map.doors.push_back(Door(7, { 60, 60 }, 0));
-	_map.doors.push_back(Door(8, { 40, 60 }, 0));
-	_map.doors.push_back(Door(9, { 20, 60 }, 0));
-
-	_map.doors.push_back(Door(10, { 0, 40 }, 90));
-	_map.doors.push_back(Door(11, { 0, 20 }, 90));
-
-	_map.doors.push_back(Door(12, { 10, 30 }, 90));
-	_map.doors.push_back(Door(13, { 90, 30 }, 90));
+	_map = Map("../../../../map.xml");
 
 	printf("Done\nCreating devices ... ");
 
--- utils/include/Map.h
+++ utils/include/Map.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <glm/glm.hpp>
 #include <cinder/Shape2d.h>
+#include <cinder/Xml.h>
 
 struct Door {
 	int id;
@@ -21,4 +22,49 @@ struct Map {
 	std::vector<ci::Shape2d> outBounds;
 	std::vector<DeviceLoc> devs;
 	std::vector<Door> doors;
+
+	ci::Shape2d makeRect(glm::vec2 topLeft, float width, float height) {
+		ci::Shape2d shape;
+		shape.moveTo(topLeft);
+		shape.lineTo(glm::vec2(topLeft.x + width, topLeft.y));
+		shape.lineTo(glm::vec2(topLeft.x + width, topLeft.y + height));
+		shape.lineTo(glm::vec2(topLeft.x, topLeft.y + height));
+		shape.close();
+		return shape;
+	}
+
+	void loadBounds(ci::XmlTree map,  std::string name, std::vector<ci::Shape2d>& bounds) {
+		for (ci::XmlTree::Iter child = map.begin(name); child != map.end(); ++child) {
+			float x = child->getAttributeValue<float>("x");
+			float y = child->getAttributeValue<float>("y");
+			float width = child->getAttributeValue<float>("width");
+			float height = child->getAttributeValue<float>("height");
+			bounds.push_back(makeRect({ x, y }, width, height));
+		}
+	}
+
+	Map() {}
+
+	Map(std::string filename) {
+		ci::XmlTree doc(ci::loadFile(filename));
+		ci::XmlTree map = doc.getChild("map");
+		size.x = map.getAttributeValue<float>("width");
+		size.y = map.getAttributeValue<float>("height");
+		loadBounds(map, "inBound", inBounds);
+		loadBounds(map, "outBound", outBounds);
+		for (ci::XmlTree::Iter child = map.begin("device"); child != map.end(); ++child) {
+			int id = child->getAttributeValue<int>("id");
+			float x = child->getAttributeValue<float>("x");
+			float y = child->getAttributeValue<float>("y");
+			float angle = child->getAttributeValue<float>("direction");
+			devs.push_back(DeviceLoc{id, {x, y},  angle});
+		}
+		for (ci::XmlTree::Iter child = map.begin("door"); child != map.end(); ++child) {
+			int id = child->getAttributeValue<int>("id");
+			float x = child->getAttributeValue<float>("x");
+			float y = child->getAttributeValue<float>("y");
+			float angle = child->getAttributeValue<float>("angle");
+			doors.push_back(Door{ id, {x, y}, angle });
+		}
+	}
 };
