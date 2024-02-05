04c2f35cd3885c3f5ce4679541e201c2a4affc48
PaulBailey-1 <paul.ryan.bailey@gmail.com>
2024-02-04 21:55:11 -0600

    Visualizer working in 3d

--- visualizer/include/VisualizerApp.h
+++ visualizer/include/VisualizerApp.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include <vector>
+#include <queue>
 
 #include <cinder/app/App.h>
 #include <cinder/app/RendererGl.h>
 #include <cinder/gl/gl.h>
+#include "cinder/Camera.h"
+#include "cinder/GeomIo.h"
+#include "cinder/CameraUi.h"
 
 #include <utils/EntityState.h>
 
@@ -21,6 +25,8 @@ public:
 	void setup() override;
 	void update() override;
 	void draw() override;
+	void mouseDown(ci::app::MouseEvent event) override;
+	void mouseDrag(ci::app::MouseEvent event) override;
 	void cleanup() override;
 
 private:
@@ -31,14 +37,25 @@ private:
 	ci::Font _font;
 
 	std::vector<Face> _faces;
-	FFMat _distances;
+	Eigen::MatrixXd _distances;
 	Eigen::MatrixXd _state;
 	Eigen::MatrixXd _stateGradient;
 	double _stepSize {0.01};
 	int _nDim {0};
+	double _maxDistance{ 0.0 };
+	double _zoom{ 4.0 };
 	int _time { 0 };
+	bool _done{ false };
+
+	std::queue<double> _pastCosts;
+	double _avgCostSum{ 0.0 };
 
 	std::ofstream _log;
+
+	ci::CameraPersp _cam;
+	ci::CameraUi _camUi;
+
+	ci::gl::BatchRef _nodeShape;
 
 };
 
--- visualizer/src/VisualizerApp.cpp
+++ visualizer/src/VisualizerApp.cpp
@@ -11,121 +11,175 @@ VisualizerApp::VisualizerApp() {
 void VisualizerApp::setup() {
 	_font = ci::Font("Times New Roman", 18);
 
-	loadData("../../../../datasetSmall.csv", 4, 12);
+	_camUi = ci::CameraUi(&_cam);
+
+	auto lambert = ci::gl::ShaderDef().lambert().color();
+	ci::gl::GlslProgRef shader = ci::gl::getStockShader(lambert);
+
+	auto nodeShape = ci::geom::Sphere().radius(0.5f).subdivisions(3);
+	_nodeShape = ci::gl::Batch::create(nodeShape, shader);
+
+
+	loadData("../../../../dataset.csv", 9, 12);
+	//loadData("../../../../datasetSmall.csv", 4, 12);
+	//loadData("../../../../datasetTiny.csv", 2, 1);
+
 	//Face face1;
 	//face1.entity = 0;
-	//face1.features << 500.0, 100.0, Eigen::VectorXf::Zero(126);
+	//face1.features << 10.5, 5.2, Eigen::VectorXf::Zero(126);
 	//_faces.push_back(face1);
 
 	//Face face2;
 	//face2.entity = 0;
-	//face2.features << 150.0, 200.0, Eigen::VectorXf::Zero(126);
+	//face2.features << 10.8, 10.1, Eigen::VectorXf::Zero(126);
 	//_faces.push_back(face2);
 
 	//Face face3;
 	//face3.entity = 1;
-	//face3.features << 350.0, 600.0, Eigen::VectorXf::Zero(126);
+	//face3.features << 1.5, 4.9, Eigen::VectorXf::Zero(126);
 	//_faces.push_back(face3);
 
 	//Face face4;
 	//face4.entity = 1;
-	//face4.features << 100.0, 50.0, Eigen::VectorXf::Zero(126);
+	//face4.features << 5.4, 2.1, Eigen::VectorXf::Zero(126);
 	//_faces.push_back(face4);
 
-	//_nDim = _faces.size();
+	_nDim = _faces.size();
 
 	// Compute distances
-	_distances(_nDim, _nDim);
+	_distances = Eigen::MatrixXd::Zero(_nDim, _nDim);
 	for (int i = 0; i < _nDim; i++) {
-		_distances(i, i) = 0.0;
 		for (int j = i + 1; j < _nDim; j++) {
 			_distances(i, j) = l2Distance(_faces[i].features, _faces[j].features);
 		}
 	}
 	// _distances = _distances.selfadjointView<Upper>();
+	_maxDistance = _distances.maxCoeff();
 
 	// Randomize state
 	srand(56711176);
-	_state = Eigen::MatrixXd::Random(_nDim, 2);
-	_state = _state * _distances.maxCoeff() / 2.0;
-	_stateGradient = Eigen::MatrixXd(_nDim, 2);
+	_state = Eigen::MatrixXd::Random(_nDim, 3);
+	_state = _state * _maxDistance / 2.0;
+	_stateGradient = Eigen::MatrixXd(_nDim, 3);
 
 	_log.open("log.csv");
 	_log << "timestep, cost, step, gradient\n";
 
 	CI_LOG_D("Beginning optimization ...");
+
 }
 
 void VisualizerApp::update() {
 
-	// compute gradient
-	for (int i = 0; i < _nDim; i++) {
-		double xSum = 0.0, ySum = 0.0;
-		for (int j = i + 1; j < _nDim; j++) {
-			double a = 1 - _distances(i, j) / (_state.row(i) - _state.row(j)).norm();
-			xSum += (_state(i, 0) - _state(j, 0)) * a;
-			ySum += (_state(i, 1) - _state(j, 1)) * a;
+	if (!_done) {
+		// compute gradient
+		for (int i = 0; i < _nDim; i++) {
+			Eigen::VectorXd sum = Eigen::VectorXd::Zero(_state.cols());
+			for (int j = i + 1; j < _nDim; j++) {
+				double a = 1 - _distances(i, j) / (_state.row(i) - _state.row(j)).norm();
+				sum += (_state.row(i) - _state.row(j)) * a;
+			}
+			_stateGradient.row(i) = 2.0 * sum;
 		}
-		_stateGradient(i, 0) = 2.0 * xSum;
-		_stateGradient(i, 1) = 2.0 * ySum;
+		double gradientAvg = _stateGradient.sum() / double(_stateGradient.rows() * _stateGradient.cols());
+
+		// gradient decent update
+		_state = _state - _stepSize * _stateGradient;
+
+		// step size freezing
+		_stepSize = 0.999 * _stepSize;
+
+		//fmt::println("Cost: {}", computeCost());
+		_time++;
+		double cost = computeCost();
+
+		CI_LOG_D("Cost: " + std::to_string(cost));
+		_log << time << ", " << cost << ", " << _stepSize << ", " << gradientAvg << "\n";
+
+		glm::vec3 center = { _state.col(0).sum(), _state.col(1).sum(), _state.col(2).sum() };
+		center = center / (float)_nDim;
+		_cam.setFarClip((_zoom + 0.5) * _maxDistance);
+		_cam.lookAt(center + glm::vec3{ _zoom * _maxDistance, 0, 0 }, center);
+
+		_pastCosts.push(cost);
+		_avgCostSum += cost;
+		if (_pastCosts.size() > 100) {
+			_avgCostSum -= _pastCosts.front();
+			_pastCosts.pop();
+		}
+		double avgCost = _avgCostSum / _pastCosts.size();
+
+		if (cost > avgCost) {
+			_done = true;
+			CI_LOG_D("Optimization complete");
+		}
 	}
-	double gradientAvg = _stateGradient.sum() / double(_nDim * 2);
-
-	// gradient decent update
-	_state = _state - _stepSize * _stateGradient;
-
-	// step size freezing
-	_stepSize = 0.999 * _stepSize;
-
-	//fmt::println("Cost: {}", computeCost());
-	double cost = computeCost();
-	CI_LOG_D("Cost: " + std::to_string(cost));
-	_log << time << ", " << cost << ", " << _stepSize << ", " << gradientAvg << "\n";
-	_time++;
 }
 
 void VisualizerApp::draw() {
 	static ci::TextBox tboxBase = ci::TextBox().alignment(ci::TextBox::CENTER).font(_font).size(glm::ivec2(50, 15));
 
 	ci::gl::clear(ci::Color::white());
-	ci::gl::setMatricesWindow(getWindowSize());
-	//ci::gl::enableAlphaBlending();
+	ci::gl::enableDepthRead();
+	ci::gl::enableDepthWrite();
 
-	ci::gl::translate({getWindowSize().x / 2.0, getWindowSize().y / 2.0});
-	double xRange = getWindowSize().x /  (_state.col(0).maxCoeff() - _state.col(0).minCoeff());
-	double yRange = getWindowSize().y / (_state.col(1).maxCoeff() - _state.col(1).minCoeff());
-	double scale;
-	if (xRange > yRange) {
-		scale = xRange;
-	} else {
-		scale = yRange;
-	}
-	scale *= 0.3;
-	ci::gl::scale(scale, scale);
+	ci::gl::setMatrices(_cam);
 
+	//ci::gl::setMatricesWindow(getWindowSize());
+	////ci::gl::enableAlphaBlending();
+
+	//ci::gl::translate({getWindowSize().x / 2.0, getWindowSize().y / 2.0});
+	//double xRange = getWindowSize().x /  (_state.col(0).maxCoeff() - _state.col(0).minCoeff());
+	//double yRange = getWindowSize().y / (_state.col(1).maxCoeff() - _state.col(1).minCoeff());
+	//double xRange = _state.col(0).maxCoeff() - _state.col(0).minCoeff();
+	//double yRange = _state.col(1).maxCoeff() - _state.col(1).minCoeff();
+	//double scale;
+	//if (xRange > yRange) {
+	//	scale = xRange;
+	//} else {
+	//	scale = yRange;
+	//}
+	//ci::gl::scale(scale, scale);
+
+	//if (_done) {
+	//	for (int i = 0; i < _nDim; i++) {
+	//		glm::vec3 pos = { _state(i, 0), _state(i, 1), _state(i, 2) };
+	//		for (int j = i + 1; j < _nDim; j++) {
+	//			ci::gl::color(ci::ColorA(0.0, 0.0, 0.0, exp(-5 * _distances(i, j) / _maxDistance)));
+	//			ci::gl::drawLine(pos, { _state(j, 0), _state(j, 1), _state(j, 2) });
+
+	//			//ci::gl::pushModelMatrix();
+	//			//ci::gl::scale(1 / scale, 1 / scale);
+	//			//double error = (_state.row(i) - _state.row(j)).norm() - _distances(i, j);
+	//			//ci::gl::Texture2dRef textTexture = ci::gl::Texture2d::create(tboxBase.text(std::to_string(error)).render());
+	//			//glm::vec2 mid = glm::vec2{ _state(j, 0), _state(j, 1) } - pos;
+	//			//mid /= 2.0;
+	//			//mid += pos;
+	//			//ci::gl::draw(textTexture, mid * (float)scale);
+	//			//ci::gl::popModelMatrix();
+	//		}
+	//	}
+	//}
+
+	double scale = _maxDistance / (_zoom * 3.0);
 	for (int i = 0; i < _nDim; i++) {
-		glm::vec2 pos = { _state(i, 0), _state(i, 1) };
-		for (int j = i + 1; j < _nDim; j++) {
-			ci::gl::color(ci::Color::black());
-			ci::gl::drawLine(pos, { _state(j, 0), _state(j, 1) });
-
-			//ci::gl::pushModelMatrix();
-			//ci::gl::scale(1 / scale, 1 / scale);
-			//double error = (_state.row(i) - _state.row(j)).norm() - _distances(i, j);
-			//ci::gl::Texture2dRef textTexture = ci::gl::Texture2d::create(tboxBase.text(std::to_string(error)).render());
-			//glm::vec2 mid = glm::vec2{ _state(j, 0), _state(j, 1) } - pos;
-			//mid /= 2.0;
-			//mid += pos;
-			//ci::gl::draw(textTexture, mid * (float)scale);
-			//ci::gl::popModelMatrix();
-		}
-	}
-
-	for (int i = 0; i < _nDim; i++) {
+		ci::gl::ScopedModelMatrix model;
 		ci::gl::color(ci::Color(ci::CM_HSV, _faces[i].entity / 15.0, 1, 1));
-		glm::vec2 pos = { _state(i, 0), _state(i, 1) };
-		ci::gl::drawSolidCircle(pos, 10.0 / scale);
+		glm::vec3 pos = { _state(i, 0), _state(i, 1),  _state(i, 2) };
+		ci::gl::translate(pos);
+		ci::gl::scale(scale, scale, scale);
+		_nodeShape->draw();
+		//ci::gl::drawSolidCircle(pos, 10.0 / scale);
 	}
+
+}
+
+void VisualizerApp::mouseDown(ci::app::MouseEvent event) {
+	_camUi.mouseDown(event);
+}
+
+void VisualizerApp::mouseDrag(ci::app::MouseEvent event) {
+	_camUi.mouseDrag(event);
 }
 
 void VisualizerApp::cleanup() {
